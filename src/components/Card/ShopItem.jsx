fb805f6cfd6d584f84cbe03c6ee001b9cd2b313d
N.pushkar <pushkar.anastasiia@gmail.com>
2024-12-30 18:36:21 +0300

    initial commit

---
+++ src/components/Card/ShopItem.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './ShopItem.css'
+
+export const ShopItem = (props) => {
+  const {brand, title, description, descriptionFull, price, currency} = props.info
+  return <div className="main-content">
+      <h2>{brand}</h2>
+      <h1>{title}</h1>
+      <h3>{description}</h3>
+      <div className="description">{descriptionFull}</div>
+      <div className="highlight-window mobile "><div class="highlight-overlay"></div></div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+        <div className="price">{currency}{price}</div>
+        <button>Добавить в корзину</button>
+      </div>
+    </div>
+}
